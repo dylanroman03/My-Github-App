3a1c555f508cd937b26aca5ceea2a0feea21c06f
dylanroman03 <romand.buitrago@gmail.com>
2019-09-15 20:27:52 -0400

    Function ajaxGet agregada

---
+++ ajax.js
@@ -0,0 +1,19 @@
+function ajaxGet(url, callback){
+	var req = new XMLHttpRequest();
+	req.open("GET", url, true);
+	req.addEventListener("load", function(){
+		if(req.status >= 200 && req.status < 400){
+			callback(req.responseText);
+		}else {
+			//Error de Servidor
+			document.write(req.status + " " + req.statusText)
+		}
+	});
+
+	//Error de Red
+	req.addEventListener("error", function(){
+		document.write("Error de Red")
+	});
+
+	req.send(null);
+}
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-var exploreFormElt = document.getElementById("exploreForm")
+var exploreFormElt = document.getElementById("exploreForm");
+var exploreInputElt = document.getElementById("exploreInput");
 
-exploreFormElt.addEventListener("sumbit", function(){
-
-})
+exploreFormElt.addEventListener("submit", function(e){
+	e.preventDefault();
+	var nameUser = exploreInputElt.value;
+	var url = "https://api.github.com/users/" + nameUser
+	ajaxGet(url, function(reply){
+		var user = JSON.parse(reply);
+		//mostrarInformacion(user)
+		console.log(user);
+	});
+});
