c4e9479339a9614e7fa6050eddc4862beb4f9328
dylanroman03 <romand.buitrago@gmail.com>
2019-09-17 13:12:42 -0400

    recibiendo datos de usuario

--- main.js
+++ main.js
@@ -7,7 +7,65 @@ exploreFormElt.addEventListener("submit", function(e){
 	var url = "https://api.github.com/users/" + nameUser
 	ajaxGet(url, function(reply){
 		var user = JSON.parse(reply);
-		//mostrarInformacion(user)
+		viewInformation(user);
 		console.log(user);
 	});
 });
+
+//Function View Information para mostrar informacion
+function viewInformation(user){
+	var divInformationElt = document.getElementById("information");
+	
+	//Avatar
+	var imgAvatarElt = document.createElement("img");
+	imgAvatarElt.src = user.avatar_url;
+	divInformationElt.appendChild(imgAvatarElt);
+	
+	//Name (Nombre real)
+        var nameElt = document.createElement("h1");
+        nameElt.textContent = user.name;
+        divInformationElt.appendChild(nameElt)	;
+
+	//Login (Nombre de usuario)
+	var loginElt = document.createElement("h2");
+	loginElt.textContent = user.login;
+	divInformationElt.appendChild(loginElt);
+
+	//Bio
+	var bioElt = document.createElement("p");
+	bioElt.textContent = user.bio;
+	divInformationElt.appendChild(bioElt);
+
+	//Button repos
+	var buttonRepos = document.createElement("button");
+	buttonRepos.textContent = "repositorios";
+	divInformationElt.appendChild(buttonRepos);
+
+	buttonRepos.addEventListener("click", function(e){
+        	var url = "https://api.github.com/users/" + user.login + "/repos"
+	        ajaxGet(url, function(reply){
+               		var repos = JSON.parse(reply);
+                	//viewInformation(user);
+                	console.log(repos);
+        	});
+
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
